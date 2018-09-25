Lochnair/kernel_e300_old

Add Jenkinsfile

Jenkinsfile
@@ -1,0 +1,53 @@
+pipeline {
+    agent {
+        docker { image 'lochnair/octeon-buildenv:latest' }
+    }
+
+    stages {
+        stage('Clean') {
+            steps {
+               sh 'git reset --hard'
+               sh 'git clean -fX'
+            }
+        }
+
+        stage('Prepare for out-of-tree builds') {
+            steps {
+                sh 'make -j5 ARCH=mips CROSS_COMPILE=mips64-octeon-linux- prepare modules_prepare'
+                sh 'rm -rf tmp && mkdir tmp'
+                sh 'tar --exclude-vcs --exclude=tmp -cf tmp/e300-ksrc.tar .'
+                sh 'mv tmp/e300-ksrc.tar e300-ksrc.tar'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'make -j5 ARCH=mips CROSS_COMPILE=mips64-octeon-linux- vmlinux modules'
+            }
+        }
+        
+        stage('Archive kernel image') {
+            steps {
+                sh 'mv -v vmlinux vmlinux.64'
+                sh 'tar cvjf e300-kernel.tar.bz2 vmlinux.64'
+                archiveArtifacts artifacts: 'e300-kernel.tar.bz2', fingerprint: true, onlyIfSuccessful: true
+            }
+        }
+        
+        stage('Archive kernel modules') {
+            steps {
+                sh 'make ARCH=mips CROSS_COMPILE=mips64-octeon-linux- INSTALL_MOD_PATH=destdir modules_install'
+                sh 'tar cvjf e300-modules.tar.bz2 -C destdir .'
+                archiveArtifacts artifacts: 'e300-modules.tar.bz2', fingerprint: true, onlyIfSuccessful: true
+            }
+        }
+
+        stage('Archive build tree') {
+            steps {
+                sh 'tar -uvf e300-ksrc.tar Module.symvers'
+                sh 'bzip2 e300-ksrc.tar'
+                archiveArtifacts artifacts: 'e300-ksrc.tar.bz2', fingerprint: true, onlyIfSuccessful: true
+            }
+        }
+    }
+}

Lochnair/kernel_ugw3

Jenkinsfile
@@ -1,0 +1,53 @@
+pipeline {
+    agent {
+        docker { image 'lochnair/octeon-buildenv:latest' }
+    }
+
+    stages {
+        stage('Clean') {
+            steps {
+               sh 'git reset --hard'
+               sh 'git clean -fX'
+            }
+        }
+
+        stage('Prepare for out-of-tree builds') {
+            steps {
+                sh 'make -j5 ARCH=mips CROSS_COMPILE=mips64-octeon-linux- prepare modules_prepare'
+                sh 'rm -rf tmp && mkdir tmp'
+                sh 'tar --exclude-vcs --exclude=tmp -cf tmp/ugw3-ksrc.tar .'
+                sh 'mv tmp/ugw3-ksrc.tar ugw3-ksrc.tar'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh 'make -j5 ARCH=mips CROSS_COMPILE=mips64-octeon-linux- vmlinux modules'
+            }
+        }
+        
+        stage('Archive kernel image') {
+            steps {
+                sh 'mv -v vmlinux vmlinux.64'
+                sh 'tar cvjf ugw3-kernel.tar.bz2 vmlinux.64'
+                archiveArtifacts artifacts: 'ugw3-kernel.tar.bz2', fingerprint: true, onlyIfSuccessful: true
+            }
+        }
+        
+        stage('Archive kernel modules') {
+            steps {
+                sh 'make ARCH=mips CROSS_COMPILE=mips64-octeon-linux- INSTALL_MOD_PATH=destdir modules_install'
+                sh 'tar cvjf ugw3-modules.tar.bz2 -C destdir .'
+                archiveArtifacts artifacts: 'ugw3-modules.tar.bz2', fingerprint: true, onlyIfSuccessful: true
+            }
+        }
+
+        stage('Archive build tree') {
+            steps {
+                sh 'tar -uvf ugw3-ksrc.tar Module.symvers'
+                sh 'bzip2 ugw3-ksrc.tar'
+                archiveArtifacts artifacts: 'ugw3-ksrc.tar.bz2', fingerprint: true, onlyIfSuccessful: true
+            }
+        }
+    }
+}